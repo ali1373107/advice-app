58c0b4fb63c29683643749c394b4efa5cfcd5c80
Ali Dashti <alidashti1994a@gmail.com>
2025-12-12 14:14:42 +0000

    adding count State

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import {useState} from "react";
 
 export default function App() {
     const [advice, setAdvice] = useState("");
+    const [count , setCount] = useState(0)
 
     async function getAdvice(){
         const res = await fetch("https://api.adviceslip.com/advice");
         const data = await res.json();
-        setAdvice(data.slip.advice)
+        setAdvice(data.slip.advice);
+        setCount((c)=> c+1);
     }    
   return(
     <div>
         <h1>{advice}</h1>
         <button onClick={getAdvice}>Click Me</button>
+        <p>You Clicked <strong>{count}</strong>Times</p>
           </div>
+
   );
 }
